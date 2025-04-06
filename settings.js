6931403f45175224e172e4de6c58ba2167166c86
Muhammad Waseem <waseemuxui@gmail.com>
2025-04-07 00:03:09 +0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
   packname: 'Knight',
-  author: 'Bot',
-  botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '917023951514', //Your number
+  author: 'Mr Waseem',
+  botName: "Shafi IT Solutions",
+  botOwner: 'Mr Waseem UXUi', // Your name
+  ownerNumber: '923341122126', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
