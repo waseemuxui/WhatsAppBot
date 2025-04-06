3a76292fda738b7adac8d3497ce0351b84a227ba
Muhammad Waseem <waseemuxui@gmail.com>
2025-04-07 01:03:38 +0500

    Update autostatus.js

--- commands/autostatus.js
+++ commands/autostatus.js
@@ -8,7 +8,7 @@ const channelInfo = {
         isForwarded: true,
         forwardedNewsletterMessageInfo: {
             newsletterJid: '120363161513685998@newsletter',
-            newsletterName: 'KnightBot MD',
+            newsletterName: 'SHAFI IT BOT by Mr Waseem',
             serverMessageId: -1
         }
     }
